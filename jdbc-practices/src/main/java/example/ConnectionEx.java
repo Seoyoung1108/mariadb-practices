443c38a7fc0c794e03b9ba8c4c1cc7fc57d77fb4
BIT <BIT@BIT1>
2024-12-12 16:30:52 +0900

    jdbc 12/12

---
+++ jdbc-practices/src/main/java/example/ConnectionEx.java
@@ -0,0 +1,35 @@
+package example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionEx {
+
+	public static void main(String[] args) {
+		Connection conn=null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.153:3306/webdb";
+			conn = DriverManager.getConnection(url,"webdb","webdb");
+			
+			System.out.println("연결성공!!!");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: "+e);
+		} catch (SQLException e) {
+			System.out.println("error: "+e);
+		} finally {
+			try {
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
